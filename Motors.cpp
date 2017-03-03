0a303ed29f3f4c1614fa64e9fe8f2cfb86a6e223
Eni Asebiomo <eniolufe@stanford.edu>
2017-03-02 17:18:16 -0800

    Added header files

---
+++ Motors.cpp
@@ -0,0 +1,91 @@
+/*=============================================================================
+
+Module
+Motors.cpp
+Description
+Library for motors
+
+=============================================================================*/
+
+/*---------------------------Dependencies------------------------------------*/
+#include "Motors.h"
+
+
+/*---------------------------Module Variables--------------------------------*/
+
+char motorPins[MOTORS][PINS_PER_MOTOR] = {
+  {PIN_MLEFT_EN, PIN_MLEFT_A, PIN_MLEFT_B},
+  {PIN_MRIGHT_EN, PIN_MRIGHT_B, PIN_MRIGHT_A},
+  {PIN_MFRONT_EN, PIN_MFRONT_A, PIN_MFRONT_B},
+  {PIN_MBACK_EN, PIN_MBACK_B, PIN_MBACK_A}
+};
+char motorSpeeds[MOTORS];
+
+/*===========================Module Code=====================================*/
+
+void applyMotorSettings() {
+  for(int i = 0;i < MOTORS;i++) {
+    // write speed
+    analogWrite(motorPins[i][0], abs(motorSpeeds[i]) * 2);
+    // write direction
+    digitalWrite(motorPins[i][1], getMotorForward(i) ? HIGH : LOW);
+    digitalWrite(motorPins[i][2], getMotorForward(i) ? LOW : HIGH);
+  }
+}
+
+bool getMotorForward(int motor) {
+  return motorSpeeds[motor] > 0;
+}
+
+char getMotorSpeed(int motor) {
+  return motorSpeeds[motor];
+}
+
+void setMotorSpeed(int motor, char val) {
+  motorSpeeds[motor] = constrain(val, -127, 127);
+}
+
+void moveLeft(char val) {
+  char speedVal = constrain(val, -127, 127);
+  motorSpeeds[MFRONT] = -speedVal;
+  motorSpeeds[MBACK] = -speedVal;
+}
+
+void moveRight(char val) {
+  char speedVal = constrain(val, -127, 127);
+  motorSpeeds[MFRONT] = speedVal;
+  motorSpeeds[MBACK] = speedVal;
+}
+
+void moveBack(char val) {
+  char speedVal = constrain(val, -127, 127);
+  motorSpeeds[MLEFT] = -speedVal;
+  motorSpeeds[MRIGHT] = -speedVal;
+}
+
+void moveForward(char val) {
+  char speedVal = constrain(val, -127, 127);
+  motorSpeeds[MLEFT] = speedVal;
+  motorSpeeds[MRIGHT] = speedVal;
+}
+
+void stopDriveMotors(void) {
+  for(int i = 0;i < MOTORS;i++) {
+    motorSpeeds[i] = 0;
+  }
+}
+
+void flipMotorDirection(int motor) {
+  motorSpeeds[motor] = map(motorSpeeds[motor], -127, 127, 127, -127);
+}
+
+void setupMotorPins(void) {
+  pinMode(PIN_POT, INPUT);
+
+  for(int i = 0;i < MOTORS;i++) {
+    for(int j = 0;j < PINS_PER_MOTOR;j++) {
+      pinMode(motorPins[i][j], OUTPUT);
+    }
+  }
+}
+
---
+++ Motors.h
@@ -0,0 +1,56 @@
+/*=============================================================================
+ 
+  Motors.h
+
+=============================================================================*/
+
+#include "Arduino.h"
+
+/*---------------Module Defines-----------------------------*/
+
+// Pinout
+#define PIN_POT 3           	// Input pin for potentiometer
+
+#define PIN_MBACK_EN 11    	// Output pin for H Bridge
+#define PIN_MBACK_A 12			// One side of drive
+#define PIN_MBACK_B 13			// Other side of drive
+
+#define PIN_MRIGHT_EN 10
+#define PIN_MRIGHT_A 9
+#define PIN_MRIGHT_B 8
+
+#define PIN_MFRONT_EN 5
+#define PIN_MFRONT_A 6
+#define PIN_MFRONT_B 7
+
+#define PIN_MLEFT_EN 3
+#define PIN_MLEFT_A 2
+#define PIN_MLEFT_B 4
+
+// Constants
+#define MOTORS 4
+#define PINS_PER_MOTOR 3
+
+// Timers
+#define TIMER_0 0
+#define INTERVAL_0 2000
+
+/*---------------------------Public Functions--------------------------------*/
+// Motor Reference
+enum motorID {
+  MLEFT,
+  MRIGHT,
+  MFRONT,
+  MBACK
+};
+
+void applyMotorSettings(void);
+bool getMotorForward(int motor);
+char getMotorSpeed(int motor);
+void moveLeft(char val);
+void moveRight(char val);
+void moveBack(char val);
+void moveForward(char val);
+void stopDriveMotors(void);
+void flipMotorDirection(int motor);
+void setupMotorPins(void);
