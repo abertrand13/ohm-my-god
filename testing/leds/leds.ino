90cc5c9aee54b423ec6dd013ac7835fe60e83426
Eni Asebiomo <eniolufe@stanford.edu>
2017-03-08 03:21:25 -0800

    Cleans up code and adds led testing

---
+++ testing/leds/leds.ino
@@ -0,0 +1,18 @@
+#define YELLOW A0
+#define RED A1
+#define GREEN A2
+
+
+void setup() {
+	pinMode(YELLOW, OUTPUT);
+	pinMode(RED, OUTPUT);
+	pinMode(GREEN, OUTPUT);
+
+	digitalWrite(YELLOW, HIGH);
+	digitalWrite(RED, HIGH);
+	digitalWrite(GREEN, HIGH);
+}
+
+void loop() {
+
+}
